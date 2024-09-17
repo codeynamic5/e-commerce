3f989cb703ec2a2eec6cfa65e93b6e0bef1e8ec4
Abella Maya Santi <abellajoesni@gmail.com>
2024-09-17 13:33:28 +0700

    Tugas 3 | Push github

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10 , decimal_places=3)
     description = models.TextField(max_length=500)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import ProductEntry
 from .models import Product
 
 # Create your views here.
 def show_main(request):
+    product_entries = ProductEntry.objects.all()
     
     return render(request, "main.html")
 
@@ -14,3 +17,13 @@ def show_model_main(request):
 
 def show_static_main(request):
     return render(request, "static_main.html")
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+    
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
