e8079358dba8aa698fe6f0cf8fbf973fcb4438eb
Abella Maya Santi <abellajoesni@gmail.com>
2024-09-24 09:55:23 +0700

    Tugas 4 | Penyempurnaan tugas 4

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3$_z5%ye52dl#h=f0&ltkgl7jl_y8gf9i0&ihrd32ysg8vn$j)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["*"]
 
--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
 
 class ProductEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10 , decimal_places=3)
     description = models.TextField(max_length=500)
