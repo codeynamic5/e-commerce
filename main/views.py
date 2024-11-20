a73f46ffe63f9662b4b70ad565186dda58e4b623
Abella Maya Santi <abellajoesni@gmail.com>
2024-11-20 07:01:00 +0700

    Tugas 9 | Reserved merge conlficts

--- main/views.py
+++ main/views.py
@@ -11,12 +11,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-<<<<<<< HEAD
-from django.views.decorators.http import require_POST
-=======
 import json
 from django.http import JsonResponse
->>>>>>> e77869b (Tugas 9)
 
 # Create your views here.
 @login_required(login_url='/login')
